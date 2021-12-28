8a60d9e24b55aeea1c2d9a9a3e976ac9381ab60b
sunshouxun <sunshouxun@bytedance.com>
2021-12-28 10:20:35 +0800

    feat: print privateKey

--- components/cloud-cluster/main.go
+++ components/cloud-cluster/main.go
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2021-12-23 14:10:57
+ * @LastEditors: sunshouxun
+ * @LastEditTime: 2021-12-26 15:36:26
+ * @FilePath: /ohmytiup/components/cloud-cluster/main.go
+ */
 package main
 
 import (
@@ -11,7 +17,7 @@ import (
 )
 
 func main() {
-	_, publicKey, err := utils.GenerateSSHKeys()
+	privateKey, publicKey, err := utils.GenerateSSHKeys()
 
 	initScript := fmt.Sprintf(`#!/bin/bash
 useradd -g root -d /home/tidb tidb -m
@@ -26,7 +32,7 @@ mkdir -p /tmp/bbb
 
 	sess := session.Must(session.NewSession(
 		&aws.Config{
-			Region:     aws.String("ap-northeast-1"),
+			Region:     aws.String("us-east-2"),
 			MaxRetries: aws.Int(3),
 		}))
 	// Create EC2 service client
@@ -34,23 +40,25 @@ mkdir -p /tmp/bbb
 	bcs := base64.StdEncoding.EncodeToString([]byte(initScript))
 	// Specify the details of the instance that you want to create.
 	runResult, err := svc.RunInstances(&ec2.RunInstancesInput{
-		ImageId:          aws.String("ami-09f4ee26f4eca3293"),
+		ImageId:          aws.String("ami-002068ed284fb165b"),
 		InstanceType:     aws.String("t2.micro"),
 		MinCount:         aws.Int64(1),
 		MaxCount:         aws.Int64(1),
 		UserData:         aws.String(bcs),
-		SubnetId:         aws.String("subnet-0b34922bcd1f4edef"),
-		SecurityGroupIds: []*string{aws.String("sg-00ddc5815c26c2c90")},
+		SubnetId:         aws.String("subnet-3a482e76"),
+		SecurityGroupIds: []*string{aws.String("sg-0f11e4ceb2270d1bf")},
 	})
 
 	if err != nil {
 		fmt.Println("Could not create instance", err)
 		return
 	}
+	// svc.DescribeInstances(&ec2.DescribeInstancesInput{})
 
 	//out, err := svc.DescribeInstances(&ec2.DescribeInstancesInput{
 	//	InstanceIds: []*string{aws.String("10.0.0.6")},
 	//})
 
 	fmt.Println("Created instance", *runResult.Instances[0].InstanceId)
+	fmt.Printf("privateKey, %s", privateKey)
 }
